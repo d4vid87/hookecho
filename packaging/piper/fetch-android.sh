7989376ce30874c6e6ba74b4951521316bd72b17
David May <davidmay87@gmail.com>
2026-09-14 19:10:00 -0500

    fix(android): omit eSpeak host tests

--- packaging/piper/fetch-android.sh
+++ packaging/piper/fetch-android.sh
@@ -18,6 +18,7 @@ tar -xzf "$archive" --strip-components=1 -C "$work/espeak-ng"
 # Android packages the release's precompiled espeak-ng-data. The upstream CMake data target tries
 # to execute its newly cross-compiled ARM binary on the x86 build host.
 sed -i '/include(cmake\/data.cmake)/d' "$work/espeak-ng/CMakeLists.txt"
+sed -i '/add_subdirectory(tests)/d' "$work/espeak-ng/CMakeLists.txt"
 "$root/packaging/piper/fetch.sh" web "$work/voice"
 rm -rf "$assets"
 mkdir -p "$assets"
